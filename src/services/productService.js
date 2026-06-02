17e461374dd45c50047663e4d8e8cd243b79ad74
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-02 20:50:07 +0530

    Enhance product service: add validation for price and stock, improve query filters, and refine sorting options

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,53 +1,142 @@
-import Product from "../models/Product.js"
+import Product from "../models/Product.js";
 
 export const createProductService = async (productData) => {
-    const product = await Product.create(productData);
+  if (productData.price < 0) {
+    throw new Error("Price cannot be negative");
+  }
 
-    return product
-}
+  if (productData.stock < 0) {
+    throw new Error("Stock cannot be negative");
+  }
+  const product = await Product.create(productData);
 
-export const getProductService = async () => {
-    const products = await Product.find({
-        isActive: true,
-    })
+  return product;
+};
 
-    return products;
-}
+export const getProductService = async ({
+  page,
+  limit,
+  skip,
+  category,
+  search,
+  sort,
+  minPrice,
+  maxPrice,
+}) => {
+  // base query
+  const query = {
+    isActive: true,
+
+    price: {
+      $gte: minPrice,
+      $lte: maxPrice,
+    },
+  };
+
+  // category filter
+  if (category) {
+    query.category = {
+      $regex: category,
+      $options: "i",
+    };
+  }
+
+  // search filter
+  if (search) {
+    query.title = {
+      $regex: search,
+      $options: "i",
+    };
+  }
+
+  // sorting
+  let sortOption = {
+    createdAt: -1,
+  };
+
+  if (sort === "price_asc") {
+    sortOption = {
+      price: 1,
+    };
+  }
+
+  if (sort === "price_desc") {
+    sortOption = {
+      price: -1,
+    };
+  }
+
+  if (sort === "newest") {
+    sortOption = {
+      createdAt: -1,
+    };
+  }
+
+  // products
+  const products = await Product.find(query)
+    .sort(sortOption)
+    .skip(skip)
+    .limit(limit);
+
+  // total count
+  const totalProducts = await Product.countDocuments(query);
+
+  const totalPages = Math.ceil(totalProducts / limit);
+
+  return {
+    products,
+    pagination: {
+      page,
+      limit,
+      totalProducts,
+      totalPages,
+    },
+  };
+};
 
 export const getProductByIdService = async (productId) => {
-    const productById = await Product.findOne({
-        _id: productId,
-        isActive: true,
-    });
+  const productById = await Product.findOne({
+    _id: productId,
+    isActive: true,
+  });
 
-    return productById;
-}
+  return productById;
+};
 
 export const updateProductService = async (productId, updateData) => {
-    const updatedProduct = await Product.findOneAndUpdate(
-        {
-            _id: productId,
-            isActive: true,
-        },
-        updateData,
-        {
-            new:true,
-            runValidators: true,
-        }
-    )
-    return updatedProduct;
-}
+  // validations
+  if (updateData.price !== undefined && updateData.price < 0) {
+    throw new Error("Price cannot be negative");
+  }
+
+  if (updateData.stock !== undefined && updateData.stock < 0) {
+    throw new Error("Stock cannot be negative");
+  }
+
+  const updatedProduct = await Product.findOneAndUpdate(
+    {
+      _id: productId,
+      isActive: true,
+    },
+    updateData,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+  return updatedProduct;
+};
 
 export const deleteProductService = async (productId) => {
   const deletedProduct = await Product.findByIdAndUpdate(
     productId,
     {
-        isActive: false,
+      isActive: false,
     },
     {
-        new: true,
-    }
-);
+      new: true,
+    },
+  );
 
   return deletedProduct;
 };
