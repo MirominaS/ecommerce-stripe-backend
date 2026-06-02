c7bc0a7b8e2167aec8195d4b02b665189c4f6d86
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-02 16:23:00 +0530

    Refactor authMiddleware: improve token handling and user validation; ensure active user status before proceeding

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,57 +2,60 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const protect = async (req, res, next) => {
-    try {
-        let token
+  try {
+    let token;
 
-        //check authorization header
-        if(
-            req.headers.authorization && req.headers.authorization.startsWith("Bearer") 
-        ) {
-            token = req.headers.authorization.split(" ")[1];
-        }
-        //no token
-        if(!token) {
-            return res.status(401).json({
-                success:false,
-                message: "Not authorized, no token"
-            })
-        }
-
-        //verify token
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        )
-
-        const user = await User.findById(decoded.id).select(
-            "-password"
-        )
-
-        if(!user) {
-            return res.status(401).json({
-                success: false,
-                message: "User not found"
-            })
-        }
-        //attach user to request
-        req.user = user;
-        next();
-    } catch (error) {
-        res.status(401).json({
-            success: false,
-            message: "No authorized",
-        })
+    //check authorization header
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
     }
-}
-
-export const adminOnly = (req,res,next) => {
-    if(req.user && req.user.role === "admin") {
-        next();
-    } else {
-        res.status(403).json({
-            success: false,
-            message: "Admin access only"
-        })
+    //no token
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, no token",
+      });
     }
-}
+
+    //verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user.isActive) {
+      return res.status(401).json({
+        success: false,
+        message: "User account disabled",
+      });
+    }
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    //attach user to request
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "No authorized",
+    });
+  }
+};
+
+export const adminOnly = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({
+      success: false,
+      message: "Admin access only",
+    });
+  }
+};
