0725b6ce72f71bee035fa6ce00acb4827cea0fec
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-05 15:59:07 +0530

    Refactor productRoutes: reorganize imports and add importProducts route for bulk product import

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,13 +1,24 @@
 import express from "express";
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from "../controllers/productController.js";
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+  importProducts,
+} from "../controllers/productController.js";
 import { protect, adminOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/",protect, adminOnly,createProduct)
-router.get("/", getProducts)
-router.get("/:id", getProductById)
-router.put("/:id", protect, adminOnly, updateProduct)
+router.post("/", protect, adminOnly, createProduct);
+router.post("/import", protect, adminOnly, importProducts);
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+
+router.put("/:id", protect, adminOnly, updateProduct);
+
 router.delete("/:id", protect, adminOnly, deleteProduct);
 
-export default router; 
+export default router;
