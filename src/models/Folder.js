50003fff2707db06e124fb4095f76c935ead5d9a
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-10 21:47:47 +0530

    Add Folder model with schema for name and description

---
+++ src/models/Folder.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const folderSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Folder = mongoose.model(
+  "Folder",
+  folderSchema
+);
+
+export default Folder;
