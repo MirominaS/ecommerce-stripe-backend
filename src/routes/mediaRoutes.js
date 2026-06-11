33fb4a0159453d224df2d2c9e43eaac1ffeeeaad
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-11 17:19:59 +0530

    Enhance mediaRoutes to include optionalAuth middleware and add getMediaAccessUrl route

--- src/routes/mediaRoutes.js
+++ src/routes/mediaRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import upload from "../middleware/upload.js"
-import { uploadImage, deleteImage, getMedia } from "../controllers/mediaController.js";
-import { protect, adminOnly } from "../middleware/authMiddleware.js";
+import { uploadImage, deleteImage, getMedia, getMediaAccessUrl } from "../controllers/mediaController.js";
+import { protect, adminOnly, optionalAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/upload",protect,adminOnly, upload.single("image"), uploadImage);
-router.get("/", getMedia)
+router.get("/", protect, getMedia)
+router.get("/:id/url", optionalAuth,getMediaAccessUrl)
 router.delete("/:id", protect, adminOnly, deleteImage)
 
 export default router;
