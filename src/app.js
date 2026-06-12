a875462f4378c68ada8d53bdc4ea92d0dbdaaf6f
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-12 22:31:35 +0530

    Refactor app.js for consistent import formatting and route declarations

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
-import cors from "cors"
-import paymentRoutes from "./routes/paymentRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import authRoutes from "./routes/authRoutes.js"
+import cors from "cors";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import adminDashboardRoutes from "./routes/adminDashboardRoutes.js"
-import emailRoutes from './routes/emailRoutes.js'
-import mediaRoutes from './routes/mediaRoutes.js'
+import adminDashboardRoutes from "./routes/adminDashboardRoutes.js";
+import emailRoutes from "./routes/emailRoutes.js";
+import mediaRoutes from "./routes/mediaRoutes.js";
 import folderRoutes from "./routes/folderRoutes.js";
+import productVariantRoutes from "./routes/productVariantRoutes.js";
+import purchaseRoutes from "./routes/purchaseRoutes.js";
 
 const app = express();
 
@@ -15,28 +17,30 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
 //payment
-app.use("/api/payments",paymentRoutes)
+app.use("/api/payments", paymentRoutes);
 
 //products
-app.use("/api/products",productRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/variants", productVariantRoutes);
+app.use("/api/purchases", purchaseRoutes);
 
 //order
 app.use("/api/orders", orderRoutes);
 
 //dashboard
-app.use("/api/admin", adminDashboardRoutes)
+app.use("/api/admin", adminDashboardRoutes);
 
-app.use('/api/email', emailRoutes);
+app.use("/api/email", emailRoutes);
 
-app.use("/api/media",mediaRoutes)
+app.use("/api/media", mediaRoutes);
 
 app.use("/api/folders", folderRoutes);
 
-app.get("/", (req,res) => {
-    res.send("Server is running")
-})
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 export default app;
